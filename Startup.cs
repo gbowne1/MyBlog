748c5510540bd3dd185facf901b03305e6eb3f58
Greg Bowne <gbowne144@gmail.com>
2023-09-07 17:51:51 -0700

    Fixed the Configuration class bug

--- Startup.cs
+++ Startup.cs
@@ -15,9 +15,15 @@ namespace MyBlog
 {
 	public class Startup
 	{
+		private IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddRazorPages();
 			services.AddDbContext<ApplicationDbContext>(options =>
