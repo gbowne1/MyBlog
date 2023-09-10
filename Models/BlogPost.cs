1f9abacfd8e499eb9911faa40aa36ae5af725503
Greg Bowne <gbowne144@gmail.com>
2023-09-09 23:46:46 -0700

    Updated BlogPost.cs Model to pick up the new Index

--- Models/BlogPost.cs
+++ Models/BlogPost.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using System.Linq;
+using MyBlog.Models;
 public class BlogPost
 {
 	[Key]
