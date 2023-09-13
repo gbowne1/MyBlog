087c6d1c215a5462b1b2c8a4840778dc34334e41
Greg Bowne <gbowne144@gmail.com>
2023-09-13 15:47:09 -0700

    Adding the About page and fixing some minor issues with the Layout

---
+++ Controller/AboutController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+public class AboutController : Controller
+{
+	public IActionResult Index()
+	{
+		var model = new AboutModel
+		{
+			Title = "About My Blog",
+			Content = "Welcome to my blog, where I share my experiences and insights on software development, programming, and coding. As a hobbyist, I bring a fresh and relatable perspective to these topics, and I aim to provide informative and educational content that helps my readers learn new skills, solve problems, and stay up-to-date with the latest trends and technologies in the field."
+		};
+
+		return View(model);
+	}
+}
---
+++ Models/AboutModel.cs
@@ -0,0 +1,5 @@
+public class AboutModel
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
