b7b3c15703e872706efb48ec269b96a849dabce1
Greg Bowne <gbowne144@gmail.com>
2024-01-08 00:12:30 -0800

    Changing the good stuff here.  Removing the Razor stuff.

--- Controller/HomeController.cs
+++ Controller/HomeController.cs
@@ -32,7 +32,7 @@ namespace MyBlog.Controllers
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+			return View(new ErrorModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 	}
 }
--- Models/ErrorModel.cs
+++ Models/ErrorModel.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace MyBlog.Models
 {
-	public class ErrorViewModel
+	public class ErrorModel
 	{
 		public string RequestId { get; set; }
 
