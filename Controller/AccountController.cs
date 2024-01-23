39c3ab42787fc430991cced7395d4254e4d42b08
Greg Bowne <gbowne144@gmail.com>
2024-01-22 22:03:42 -0800

    Finally got this to build, had to add null to AntiForgery in Login and ResetPassword to get it to work

--- Controller/AccountController.cs
+++ Controller/AccountController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-
+using MyBlog.Models;
 public class AccountController : Controller
 {
    private readonly UserManager<IdentityUser> _userManager;
@@ -14,4 +14,22 @@ public class AccountController : Controller
    }
 
    // Actions for login, logout, password management, etc.
+
+   [HttpPost]
+   public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+        }
+
+        return View(model);
+    }
 }
