aee765fb9ae9802e6c2ac5712d9ba20a255f6a4b
Gregory Bowne <gbowne144@gmail.com>
2025-04-05 03:04:49 -0700

    Update Program.cs
    Fixed routes for controllers

--- Program.cs
+++ Program.cs
@@ -12,15 +12,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Server=(localdb)\\mssqllocaldb;Database=MyBlog;Trusted_Connection=True;MultipleActiveResultSets=true";
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+                           ?? "Server=(localdb)\\mssqllocaldb;Database=MyBlog;Trusted_Connection=True;MultipleActiveResultSets=true";
     options.UseSqlServer(connectionString);
 });
 
-// Correctly register the concrete implementation of IBlogPostService
+// Register custom services for Dependency Injection
 builder.Services.AddScoped<IBlogPostService, BlogPostService>();
-
 builder.Services.AddTransient<IAboutContentService, AboutContentService>();
 
+// Configure Identity for authentication and authorization
 builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
@@ -48,6 +49,58 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+// Custom routes for specific controllers
+app.MapControllerRoute(
+    name: "about",
+    pattern: "about",
+    defaults: new { controller = "About", action = "Index" });
+
+app.MapControllerRoute(
+    name: "account",
+    pattern: "account/{action}/{id?}",
+    defaults: new { controller = "Account" });
+
+app.MapControllerRoute(
+    name: "blog",
+    pattern: "blog/{action}/{slug?}",
+    defaults: new { controller = "Blog" });
+
+app.MapControllerRoute(
+    name: "changepassword",
+    pattern: "change-password/{action}/{id?}",
+    defaults: new { controller = "ChangePassword" });
+
+app.MapControllerRoute(
+    name: "comments",
+    pattern: "comments/{action}/{id?}",
+    defaults: new { controller = "Comment" });
+
+app.MapControllerRoute(
+    name: "editor",
+    pattern: "editor/{action}/{id?}",
+    defaults: new { controller = "Editor" });
+
+app.MapControllerRoute(
+    name: "external-login",
+    pattern: "external-login/{action}/{id?}",
+    defaults: new { controller = "ExternalLogin" });
+
+app.MapControllerRoute(
+    name: "manage",
+    pattern: "manage/{action}/{id?}",
+    defaults: new { controller = "Manage" });
+
+app.MapControllerRoute(
+    name: "register",
+    pattern: "register/{action}/{id?}",
+    defaults: new { controller = "Register" });
+
+app.MapControllerRoute(
+    name: "reset-password",
+    pattern: "password/reset/{token?}",
+    defaults: new { controller = "ResetPassword", action = "Index" });
+
+// Default route (fallback for unspecified controllers)
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
