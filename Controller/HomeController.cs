c430747abe017351aab33fe010a843a2cfe3a8eb
Gregory Bowne <gbowne144@gmail.com>
2025-11-17 20:27:29 -0800

    Update HomeController.cs

--- Controller/HomeController.cs
+++ Controller/HomeController.cs
@@ -11,6 +11,11 @@ namespace MyBlog.Controllers
             return View();
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
