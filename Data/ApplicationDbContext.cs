fae54a9e6e40c8681c40445e6d221a5013a8e941
Gregory Bowne <gbowne144@gmail.com>
2025-11-19 20:11:47 -0800

    Update ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,16 +1,19 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MyBlog.Models;
+using Microsoft.AspNetCore.Identity; // Added for IdentityUser
 
 namespace MyBlog.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    // CORRECTED: Inherit from IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser> 
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
+        // BlogPosts table setup is correct
         public DbSet<BlogPost> BlogPosts { get; set; } = default!;
     }
 }
