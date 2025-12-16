52a24176a04a515453311aff36f3d2c1f6acb623
Gregory Bowne <gbowne144@gmail.com>
2025-12-16 13:33:53 -0800

    Create RegisterModel.cs

---
+++ Models/RegisterModel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.Models
+{
+    // This model is used to collect data from the registration form.
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
